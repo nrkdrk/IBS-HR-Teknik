d4d40a7ef9095cc2b3dc63190129a8e2a1334521
Berk CAN <nrkberk@gmail.com>
2017-11-25 14:28:18 +0300

    25-11-2017

--- IBS-HR/HibeListe.Designer.cs
+++ IBS-HR/HibeListe.Designer.cs
@@ -56,6 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(883, 557);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;

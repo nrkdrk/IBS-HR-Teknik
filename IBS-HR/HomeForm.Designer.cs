7b44409e99c1c925fee1a34927f8d413e5efd46f
Berk CAN <nrkberk@gmail.com>
2017-11-25 17:51:33 +0300

    25-11-2017
    Home Form Düzenlendi

--- IBS-HR/HomeForm.Designer.cs
+++ IBS-HR/HomeForm.Designer.cs
@@ -31,12 +31,21 @@
             this.panel13 = new System.Windows.Forms.Panel();
             this.label13 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
-            this.panel12 = new System.Windows.Forms.Panel();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
             this.panel11 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel13.SuspendLayout();
-            this.panel12.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel13
@@ -44,9 +53,9 @@
             this.panel13.BackColor = System.Drawing.Color.DarkSlateGray;
             this.panel13.Controls.Add(this.label13);
             this.panel13.Controls.Add(this.label12);
-            this.panel13.Location = new System.Drawing.Point(12, 12);
+            this.panel13.Location = new System.Drawing.Point(97, 15);
             this.panel13.Name = "panel13";
-            this.panel13.Size = new System.Drawing.Size(177, 117);
+            this.panel13.Size = new System.Drawing.Size(163, 90);
             this.panel13.TabIndex = 2;
             // 
             // label13
@@ -54,62 +63,138 @@
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label13.ForeColor = System.Drawing.Color.PaleTurquoise;
-            this.label13.Location = new System.Drawing.Point(59, 65);
+            this.label13.Location = new System.Drawing.Point(54, 49);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(55, 24);
             this.label13.TabIndex = 1;
             this.label13.Text = "1,156";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             // 
             // label12
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label12.ForeColor = System.Drawing.Color.PaleTurquoise;
-            this.label12.Location = new System.Drawing.Point(49, 26);
+            this.label12.Location = new System.Drawing.Point(28, 13);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(80, 24);
+            this.label12.Size = new System.Drawing.Size(110, 24);
             this.label12.TabIndex = 0;
-            this.label12.Text = "Kullanıcı";
-            // 
-            // panel12
-            // 
-            this.panel12.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.panel12.Controls.Add(this.label11);
-            this.panel12.Controls.Add(this.label10);
-            this.panel12.Location = new System.Drawing.Point(529, 12);
-            this.panel12.Name = "panel12";
-            this.panel12.Size = new System.Drawing.Size(344, 117);
-            this.panel12.TabIndex = 4;
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label11.ForeColor = System.Drawing.Color.PaleTurquoise;
-            this.label11.Location = new System.Drawing.Point(104, 65);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(128, 20);
-            this.label11.TabIndex = 1;
-            this.label11.Text = "Berk Can(admin)";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label10.ForeColor = System.Drawing.Color.PaleTurquoise;
-            this.label10.Location = new System.Drawing.Point(95, 26);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(149, 20);
-            this.label10.TabIndex = 0;
-            this.label10.Text = "nrkberk@gmail.com";
+            this.label12.Text = "Teknik Kayıt";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // panel11
             // 
             this.panel11.BackColor = System.Drawing.Color.Teal;
+            this.panel11.Controls.Add(this.panel1);
+            this.panel11.Controls.Add(this.panel4);
+            this.panel11.Controls.Add(this.panel2);
+            this.panel11.Controls.Add(this.panel13);
             this.panel11.Location = new System.Drawing.Point(12, 12);
             this.panel11.Name = "panel11";
-            this.panel11.Size = new System.Drawing.Size(490, 117);
+            this.panel11.Size = new System.Drawing.Size(863, 120);
             this.panel11.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(266, 15);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(163, 90);
+            this.panel2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.label3.Location = new System.Drawing.Point(54, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "1,156";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.label4.Location = new System.Drawing.Point(28, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Teknik Liste";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Controls.Add(this.label8);
+            this.panel4.Location = new System.Drawing.Point(435, 15);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(163, 90);
+            this.panel4.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.label7.Location = new System.Drawing.Point(54, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 24);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "1,156";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.label8.Location = new System.Drawing.Point(37, 13);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 24);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Hibe Liste";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(604, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(163, 90);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.label1.Location = new System.Drawing.Point(54, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1,156";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.label2.Location = new System.Drawing.Point(37, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Servis Liste";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // HomeForm
             // 
@@ -117,16 +202,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(885, 398);
-            this.Controls.Add(this.panel13);
-            this.Controls.Add(this.panel12);
             this.Controls.Add(this.panel11);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "HomeForm";
             this.Text = "HomeForm";
             this.panel13.ResumeLayout(false);
             this.panel13.PerformLayout();
-            this.panel12.ResumeLayout(false);
-            this.panel12.PerformLayout();
+            this.panel11.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -136,9 +224,15 @@
         private System.Windows.Forms.Panel panel13;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Panel panel12;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
